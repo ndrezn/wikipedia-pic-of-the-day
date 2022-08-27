5d0641316eb43555cfac527e114b6e80df159ab0
Nathan <accounts@nathandrezner.com>
2022-08-27 18:52:24 -0400

    Temporary removal of test for posting

--- tests/test_posts.py
+++
@@ -1,8 +0,0 @@
-import bot
-from datetime import datetime
-
-
-def test_post():
-    date = datetime(2022, 5, 17)
-    caption, image_title, article_title = bot.go(date=date, post=True, test=True)
-    assert True
