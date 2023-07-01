4e5534e91581709d6f2681c03ed5700a35376684
Nathan Drezner <nathan@drezner.xyz>
2023-07-01 17:06:07 -0400

    Remove dotenv

--- bot/__init__.py
+++ bot/__init__.py
@@ -5,12 +5,5 @@ from .post import go
 from . import post
 from . import creds
 
-import dotenv
-
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
 if not os.path.exists("nltk_data"):
     nltk.download("punkt", download_dir="nltk_data/")
